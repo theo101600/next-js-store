e468ccc735b8985d09e5ff278a4e7ba08bcad0f1
Theo <theo.lacanaria@gmail.com>
2026-03-04 16:29:58 +0800

    build navbar skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <div>Store Starter</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 };
 export default HomePage;
