ed219017e63825f4773157c98ed158205f416af7
Theo <theo.lacanaria@gmail.com>
2026-03-04 18:28:27 +0800

    implement light/dark theme and navbar dropdown menu

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FaCartShopping } from "react-icons/fa6";
+
 function CartButton() {
-  return <div>CartButton</div>;
+  // temp value
+  const numItemsInCart = 9;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <FaCartShopping />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 
 export default CartButton;
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { GiAbstract062 } from "react-icons/gi";
+import { Button } from "../ui/button";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <GiAbstract062 className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
